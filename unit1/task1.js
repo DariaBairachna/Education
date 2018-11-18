caa68fce219558159e1f988c408c0506be28155b
Daria Bairachna <daria.bairachna@gmail.com>
2018-11-18 18:36:27 +0200

    task1.2

---
+++ unit1/task1.js
@@ -0,0 +1,16 @@
+var num = 1;
+var str = "Hellow world";
+var boolType = true;
+var myKnolege = null;
+var Knolege = undefined;
+var user = {
+    age: 15,
+    name: "Vasilii"
+};
+
+alert(num);
+alert(str);
+alert(boolType);
+alert(myKnolege);
+alert(Knolege);
+alert(user);
---
+++ unit1/task2.js
@@ -0,0 +1,3 @@
+var x = 5.9568;
+var a = Math.floor(x);
+alert(a);
